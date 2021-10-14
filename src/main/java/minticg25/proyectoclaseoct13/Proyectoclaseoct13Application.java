a4d9cf6cd43ad6453d66485a2a06852dd538189f
27Angie79 <programacion.luz.rojas@gmail.com>
2021-10-14 18:13:03 -0500

    proyecto springboot

---
+++ src/main/java/minticg25/proyectoclaseoct13/Proyectoclaseoct13Application.java
@@ -0,0 +1,13 @@
+package minticg25.proyectoclaseoct13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectoclaseoct13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectoclaseoct13Application.class, args);
+	}
+
+}
---
+++ src/test/java/minticg25/proyectoclaseoct13/Proyectoclaseoct13ApplicationTests.java
@@ -0,0 +1,13 @@
+package minticg25.proyectoclaseoct13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectoclaseoct13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
